69de3d159fd42093ddbb42901ada2abed10331ab
Jérôme Poulin <jeromepoulin@gmail.com>
2015-10-08 02:52:02 -0400

    testshell.py: Re-tab, add function to start_shell.

--- testshell.py
+++ testshell.py
@@ -18,78 +18,82 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-from socketserver import BaseRequestHandler, TCPServer, ThreadingMixIn, UDPServer
+from socketserver import BaseRequestHandler, ThreadingMixIn, UDPServer
 import threading
 
 
-class ShellServer(ThreadingMixIn,UDPServer):
-    pass
+class ShellServer(ThreadingMixIn, UDPServer):
+	pass
+
 
 class ShellHandler(BaseRequestHandler):
-    clients_locals = dict()
+	client_globals = None
+	clients_locals = dict()
 
-    def handle(self):
-        #data = str(self.request.recv(1024), 'utf8')
-        #self.request.sendall(bytes(repr(ret), 'utf8'))
-        data, socket = self.request
-        client_name = repr(self.client_address)
+	def handle(self):
+		# data = str(self.request.recv(1024), 'utf8')
+		# self.request.sendall(bytes(repr(ret), 'utf8'))
+		data, socket = self.request
+		client_name = repr(self.client_address)
 
-        if client_name in ShellHandler.clients_locals:
-            c_threads, c_locals = ShellHandler.clients_locals[client_name]
+		if client_name in ShellHandler.clients_locals:
+			c_threads, c_locals = ShellHandler.clients_locals[client_name]
 
-            if data.strip() == b'.':
-                del ShellHandler.clients_locals[client_name]
-                socket.sendto(b"<RESET COMPLETE>", self.client_address)
-        else:
-            c_threads, c_locals = set(), dict()
-            ShellHandler.clients_locals[client_name] = (c_threads, c_locals)
+			if data.strip() == b'.':
+				del ShellHandler.clients_locals[client_name]
+				socket.sendto(b"<RESET COMPLETE>", self.client_address)
+		else:
+			c_threads, c_locals = set(), dict()
+			ShellHandler.clients_locals[client_name] = (c_threads, c_locals)
 
-        c_threads.add(threading.current_thread())
-        if len(data.strip()) > 0:
+		c_threads.add(threading.current_thread())
+		if len(data.strip()) > 0:
 
-            c_locals.update(locals())
-            args = (str(data, 'utf8'), globals(), c_locals)
-            try:
-                try:
-                    ret = eval(*args)
-                except SyntaxError:
-                    exec(*args)
-                    ret = "OK"
-            except Exception as e:
-                ret = repr(e)
+			c_globals = ShellHandler.client_globals
+			c_locals.update(locals())
 
-            if type(ret) != str:
-                ret = "%s %s" % (type(ret), repr(ret))
+			args = (str(data, 'utf8'), globals(), c_locals)
+			try:
+				try:
+					ret = eval(*args)
+				except SyntaxError:
+					exec(*args)
+					ret = "OK"
+			except Exception as e:
+				ret = repr(e)
 
-            socket.sendto(bytes(ret, 'utf8'), self.client_address)
+			if type(ret) != str:
+				ret = "%s %s" % (type(ret), repr(ret))
 
-        running = 0
-        job = "\nCurrently running threads:\n"
-        for t in c_threads.copy():
-            if t.is_alive():
-                job += repr(t) + "\n"
-                running += 1
-            else:
-                c_threads.discard(t)
+			socket.sendto(bytes(ret, 'utf8'), self.client_address)
 
-        if running > 2:
-            socket.sendto(bytes(job, 'utf8'), self.client_address)
+		running = 0
+		job = "\nCurrently running threads:\n"
+		for t in c_threads.copy():
+			if t.is_alive():
+				job += repr(t) + "\n"
+				running += 1
+			else:
+				c_threads.discard(t)
 
-        socket.sendto(b"\n>>> ", self.client_address)
+		if running > 2:
+			socket.sendto(bytes(job, 'utf8'), self.client_address)
+
+		socket.sendto(b"\n>>> ", self.client_address)
 
 
-def start_shell(address, port):
-    server = ShellServer((address, port), ShellHandler)
-    server_thread = threading.Thread(target=server.serve_forever)
-    server_thread.daemon = True
-    server_thread.start()
-    server.server_thread = server_thread
-    return server
+def start_shell(address, port, imported_globals=None):
+	ShellHandler.client_globals = imported_globals
+	server = ShellServer((address, port), ShellHandler)
+	server_thread = threading.Thread(target=server.serve_forever)
+	server_thread.daemon = True
+	server_thread.start()
+	server.server_thread = server_thread
+	return server
 
 if __name__ == "__main__":
-    server = start_shell('', 12123)
-    ip, port = server.server_address
-    server_thread = server.server_thread
-    print("Server loop running in thread:", server_thread.name)
+	server = start_shell('', 12123)
+	ip, port = server.server_address
+	server_thread = server.server_thread
+	print("Server loop running in thread:", server_thread.name)
 
