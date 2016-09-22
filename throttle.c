fdf80b69467093ffe7b1e52a90ee3bbe5701b8b4
Jérôme Poulin <jeromepoulin@gmail.com>
2016-09-22 04:05:04 -0400

    throttle: Add GPL license.

--- throttle.c
+++ throttle.c
@@ -1,4 +1,8 @@
 /*
+ * throttle.c allows to throttle a program by sending SIGSTOP/SIGCONT
+ * at a specified interval, this program is meant to complement cpulimit
+ * which can only limit program using CPU, a GPU program can't be limited.
+ *
  * Compile using gcc -Wall -DSYSLOG -DSYSTEMD -o throttle throttle.c -lsystemd
  *
  * Example unit file:
@@ -7,6 +11,21 @@
  * Type=notify
  * WatchdogSec=15
  * ExecStart=/bin/sh -c "exec throttle -d 0.84 -s 100000 `pidof hungry_app`"
+ *
+ * Copyright (C) 2015 Jérôme Poulin <jeromepoulin@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #include <signal.h>
