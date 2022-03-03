e0b47d26a22084ca8d2a300deefea73fb4750857
Jérôme Poulin <jeromepoulin@gmail.com>
2022-03-03 02:30:30 -0500

    make multiprocessing aware

--- mysql-print-general-log-queries.py
+++ mysql-print-general-log-queries.py
@@ -7,12 +7,15 @@ import threading
 import time
 from collections import Counter
 from queue import Queue
-from typing import Iterator
+from typing import Iterator, Optional
 
 QUERY = re.compile(rb"\s([0-9]+)\sQuery\s(.*)")
+TABLE_REGEX = re.compile(rb"\s+FROM\s+([^ (]+)\W")
+VERB_REGEX = re.compile(rb"\W([A-Z]+)\W")
+JOIN_REGEX = re.compile(rb"\s+JOIN\s+([^ (]+)\W")
 
 
-def clean_line(line: bytes):
+def clean_line(line: bytes) -> bytes:
 	return line\
 		.replace(b"  ", b" ")\
 		.replace(b"\t", b" ")\
@@ -20,7 +23,10 @@ def clean_line(line: bytes):
 		.strip()
 
 
-def get_query_line(line: bytes):
+def get_query_line(line: bytes) -> Optional[bytes]:
+	if line.find(b"Query", 12, 18) == -1:
+		return
+
 	result = QUERY.findall(line)
 
 	if len(result) == 1:
@@ -35,14 +41,14 @@ def get_log_lines(in_file: io.BytesIO) -> Iterator[bytes]:
 		if line.startswith(b"\t\t"):
 			if do_print:
 				do_print = False
-				yield b"%s\n" % (cleaned_line,)
+				yield cleaned_line + b"\n"
 
 			cleaned_line = get_query_line(line)
 
 			if cleaned_line is not None:
 				do_print = True
 		elif do_print:
-			cleaned_line += b" %s" % (clean_line(line),)
+			cleaned_line += b" " + clean_line(line)
 
 
 def clean_table_name(table_name: bytes) -> bytes:
@@ -83,13 +89,52 @@ def print_progress(current_file: Queue, total_files: int):
 	sys.stderr.write("\r            \r")
 
 
+def _init_multiprocessing(args):
+	get_verb_tables_and_joins_from_line.add_join = args.joins
+	get_verb_tables_and_joins_from_line.add_verb = args.verb
+
+
+def get_verb_tables_and_joins_from_line(line: bytes):
+	add_join = get_verb_tables_and_joins_from_line.add_join
+	add_verb = get_verb_tables_and_joins_from_line.add_verb
+	joins = set()
+	table_match = TABLE_REGEX.findall(line)
+
+	if len(table_match) == 0:
+		return
+
+	query_table_name = ""
+	out = b""
+
+	for i, table_name in enumerate(table_match):
+		if i == 0:
+			if add_verb:
+				verb_match = next(VERB_REGEX.finditer(line))
+				out += verb_match.group(1) + b" "
+			query_table_name = clean_table_name(table_name)
+			out += query_table_name
+		elif add_join:
+			joins.add(clean_table_name(table_name))
+		else:
+			break
+
+	if add_join:
+		join_match = JOIN_REGEX.findall(line)
+
+		for join in join_match:
+			joins.add(clean_table_name(join))
+
+	if joins:
+		joins.discard(query_table_name)
+		out += b" " + b" ".join(sorted(joins))
+		joins.clear()
+
+	return out
+
+
 def get_verb_tables_and_joins(args) -> Iterator[bytes]:
-	verb_regex = re.compile(rb"\W([A-Z]+)\W")
-	table_regex = re.compile(rb"\s+FROM\s+([^ (]+)\W")
-	join_regex = re.compile(rb"\s+JOIN\s+([^ (]+)\W")
 	current_file = Queue()
 	progress_thread = None
-	joins = set()
 
 	if args.progress and args.files[0] != sys.stdin:
 		progress_thread = threading.Thread(
@@ -102,45 +147,30 @@ def get_verb_tables_and_joins(args) -> Iterator[bytes]:
 
 	for f in args.files:
 		if progress_thread:
-			f.seek(0, io.SEEK_END)
-			current_file.put((f, f.tell()))
-			f.seek(0, io.SEEK_SET)
+			try:
+				f.seek(0, io.SEEK_END)
+				current_file.put((f, f.tell()))
+				f.seek(0, io.SEEK_SET)
+			except io.UnsupportedOperation:
+				current_file.put(None)
 
 		lines = get_log_lines(f)
 
-		for line in lines:
-			table_match = table_regex.findall(line)
+		if args.single_process:
+			_init_multiprocessing(args)
+			lines_iter = map(get_verb_tables_and_joins_from_line, lines)
+			yield from filter(lambda x: x is not None, lines_iter)
+		else:
+			import multiprocessing
+			p = multiprocessing.Pool(initializer=_init_multiprocessing, initargs=(args,))
 
-			if len(table_match) == 0:
-				continue
-
-			query_table_name = ""
-			out = b""
-
-			for i, table_name in enumerate(table_match):
-				if i == 0:
-					if args.verb:
-						verb_match = next(verb_regex.finditer(line))
-						out += verb_match.group(1) + b" "
-					query_table_name = clean_table_name(table_name)
-					out += query_table_name
-				elif args.joins:
-					joins.add(clean_table_name(table_name))
-				else:
-					break
-
-			if args.joins:
-				join_match = join_regex.findall(line)
-
-				for join in join_match:
-					joins.add(clean_table_name(join))
-
-			if joins:
-				joins.discard(query_table_name)
-				out += b" " + b" ".join(sorted(joins))
-				joins.clear()
-
-			yield out
+			lines_iter = p.imap_unordered(
+				get_verb_tables_and_joins_from_line,
+				iterable=lines,
+				chunksize=args.chunk_size,
+			)
+			yield from filter(lambda x: x is not None, lines_iter)
+			p.close()
 
 	if progress_thread:
 		current_file.put(None)
@@ -176,6 +206,8 @@ def command_line_parser():
 		help="includes the SQL verb at the start of the line")
 	parser_tables.add_argument("files", nargs="*", help="file(s) to parse or stdin")
 	parser_tables.set_defaults(func=cmd_print_tables)
+	parser.add_argument("-1", "--single-process", action="store_true", help="disable multiprocessing")
+	parser.add_argument("-l", "--chunk-size", type=int, default=256000, help="number of lines per processes (advanced)")
 	parser.add_argument("-p", "--progress", action="store_true", help="shows progress for some operations")
 	args = parser.parse_args()
 
